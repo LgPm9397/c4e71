0e5f4132b9963eb6e5b25b44440fa499c5367050
luan <thienchi.hongthienbac@gmail.com>
2020-02-15 22:46:53 +0700

    bat dau html-js

---
+++ session2_var/scirpt.js
@@ -0,0 +1,25 @@
+alert("xin chao C4ESJ 71");// thong bao 
+     
+prompt("ten ban la gi?");// nhap thong tin tu nguoi dung
+
+let age; // khai bao bien
+age = 27; // gans gias trij
+
+//25 = age; // sai
+
+let agel = 22;
+
+// su dung;
+// alert(age);
+// alert(agel);
+console.log(age);
+console.log(age);
+var name;
+const PI = 3.14;
+
+let a = 5;
+let b = 10;
+
+let c = a + b;
+console.log(c);
+// - */
